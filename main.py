3f8332991def2858258afc80e27ebb93d6410dcd
kobayashi hiroyuki <kobyashi@DESKTOP-4GIF3E4.localdomain>
2020-05-02 12:45:22 +0900

    return running result

--- main.py
+++ main.py
@@ -1,25 +1,85 @@
 import discord
 import os
-import request
+import requests
+import json 
+import time
+
+def run_code(code,language):
+        url = 'http://api.paiza.io:80/runners/'
+        
+        #running code
+        create = 'create'
+        post_data = {
+            "source_code": code,
+            "language":language,
+            "api_key":"guest"
+        }
+        RunnigId = requests.post(url+create, data=post_data).json()['id']
+
+        #check the status 
+        r_stat = 'running'
+        while not r_stat == 'completed':
+            time.sleep(0.1)
+            status = 'get_status'
+            status_data = {'id':RunnigId,
+                            'api_key':'guest'}
+            r_stat = requests.get(url+status,params=status_data).json()['status']
+
+        #get the successs or error result
+        details = 'get_details'
+        data = {'id':RunnigId,
+                'api_key':'guest'}
+        r_det = requests.get(url+details,params=data).json()
+        return r_det
+        
 
 class code_runner(discord.Client):
+    #ready to start up!
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_Message(self,message):
+    #when message comes!
+    async def on_message(self,message):
+        #pass when the message is own
         if message.author.bot:
             return
         
-        print('Hello')
+        #
         if message.content.startswith('!run'):
-            await message.channel.send('Hello')
+            content = message.content
+            send = message.channel.send
 
-# def run_code():
-#     url = 'http://api.paiza.io:80/runners/'
-#     cre = 'create'
-#     detail = 'get_detail'
-#     response = request.post()
+            if content.count("```") != 2:
+                return await send("SyntaxError: Missing ```")
+            
+            content = message.content.split("```")
+            row1 = content[0].split()
+            code = content[1]
+
+            if len(row1) > 2: # too long syntax
+                return await send('SyntaxError: please command as following\
+                    ```\
+                        !run <language> \
+                    ```')
+            elif len(row1) ==2:
+                language = row1[1]
+            else:
+                language = 'python3'
+
+            r_det = run_code(code,language)
+            if r_det['stderr']:
+                res = '''```
+                    {}
+                        ```'''.format(r_det['stderr'])
+                return await send(res)
+            if r_det['stdout']:
+                res = '''```
+                    {}
+                    Runnnig time: {}
+                        ```'''.format(r_det['stdout'],r_det['time'])
+                return await send(res)
+
 if __name__ == "__main__":
-    Token = os.environ["DISCORD_BOT_TOKEN"]
+    Token = os.environ.get("DISCORD_BOT_TOKEN")
     app = code_runner()
     app.run(Token)
